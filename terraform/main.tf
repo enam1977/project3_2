76826cf3747b755e027e87fb148a991142bcb6da
fengxiakira <limengyuanwinnie@163.com>
2021-08-17 13:12:33 -0400

    left: terraform apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,5 +68,6 @@ module "vm" {
   subnet_id        = "${module.network.subnet_id_test}"
   public_ip_address_id = "${module.publicip.public_ip_address_id}"
   admin_username  = var.admin_username 
+  image_id = var.image_id
 }
 
